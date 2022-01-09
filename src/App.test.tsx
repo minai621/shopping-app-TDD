02b06c43d9748270736a6f204e027aa2c4f0c374
minai621 <minai621@naver.com>
2022-01-09 17:07:36 +0900

    setup eslint, prettier, testing tools

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,4 +6,6 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+  const testlint = screen.getByTestId('test');
+  expect(testlint).toHaveTextContent('0');
 });
